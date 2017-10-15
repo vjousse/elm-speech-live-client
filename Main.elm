27b18f968d74a225b9342a4b4bbb80258fe862f8
Vincent Jousse <vincent@jousse.org>
2017-10-15 12:41:39 +0200

    Remove useless comments

--- Main.elm
+++ Main.elm
@@ -1,18 +1,5 @@
 port module Todo exposing (..)
 
-{-| TodoMVC implemented in Elm, using plain HTML and CSS for rendering.
-
-This application is broken up into three key parts:
-
-1.  Model - a full definition of the application's state
-2.  Update - a way to step the application state forward
-3.  View - a way to visualize our application state with HTML
-
-This clean division of concerns is a core part of Elm. You can read more about
-this in <http://guide.elm-lang.org/architecture/index.html>
-
--}
-
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Phoenix.Socket
@@ -37,7 +24,6 @@ main =
 
 
 -- MODEL
--- The full application state of our todo app.
 
 
 type alias Model =
@@ -53,10 +39,6 @@ type alias Model =
 -- UPDATE
 
 
-{-| Users of our app can trigger messages by clicking and typing. These
-messages are fed into the `update` function as they occur, letting us react
-to them.
--}
 type Msg
     = NoOp
     | SocketMsg (Phoenix.Socket.Msg Msg)
